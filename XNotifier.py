42f500636afeeb74db505382a22b1f57259ae0a2
Elysia-ff <jhk19960@gmail.com>
2024-05-20 19:12:08 +0900

    ignores error messages

--- XNotifier.py
+++ XNotifier.py
@@ -2,10 +2,12 @@ from subprocess import PIPE, run
 import discord
 from discord.ext import tasks
 
+USER_URL = 'https://twitter.com/airikannakr'
+USER_NAME = 'AiriKanna'
+
 class DiscordClient(discord.Client):
     async def setup_hook(self) -> None:
         self.lastMessage = ''
-        self.apiLimitExceededCount = 0
 
         self.checkXTask.start()
 
@@ -16,38 +18,20 @@ class DiscordClient(discord.Client):
     async def checkXTask(self):
         channel = self.get_channel(int(channelID))
 
-        USER_URL = 'https://twitter.com/airikannakr'
-        USER_NAME = 'AiriKanna'
         # https://github.com/HoloArchivists/twspace-dl
         result = run(f'twspace_dl --skip-download --input-cookie-file cookie.co --user-url {USER_URL} --url', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-
-        embed = discord.Embed(colour=int(1053978))
-        embed.set_author(name=USER_NAME)
 
         if result.stderr != '':
             print(result.stderr)
 
-            if result.stderr.startswith("ERROR: API rate limit exceeded"):
-                self.apiLimitExceededCount += 1
-
-                if self.apiLimitExceededCount < 3:
-                    return
-            else:
-                self.apiLimitExceededCount = 0
-
-            embed.title = 'Error'
-            embed.description = result.stderr
-            await channel.send(embed=embed)
-            # await self.close()
-
         elif result.stdout != '':
             print(result.stdout)
 
-            self.apiLimitExceededCount = 0
-
-            if self.lastMessage != result.stdout:
+            if result.stdout.rstrip().endswith('.m3u8') and self.lastMessage != result.stdout:
                 self.lastMessage = result.stdout
 
+                embed = discord.Embed(colour=int(1053978))
+                embed.set_author(name=USER_NAME)
                 embed.title = 'XSpaceNotifier'
                 embed.description = result.stdout
                 await channel.send(embed=embed)
