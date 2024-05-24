1ce2c541d200b1d33ba1d7dbaa2e19e024d52e16
Elysia-ff <jhk19960@gmail.com>
2024-05-24 21:43:24 +0900

    tracks multiple accounts now

--- XNotifier.py
+++ XNotifier.py
@@ -2,39 +2,35 @@ from subprocess import PIPE, run
 import discord
 from discord.ext import tasks
 
-USER_URL = 'https://twitter.com/airikannakr'
-USER_NAME = 'AiriKanna'
-
 class DiscordClient(discord.Client):
     async def setup_hook(self) -> None:
-        self.lastMessage = ''
-
         self.checkXTask.start()
 
     async def on_ready(self):
-        print(f'Logged on as {self.user}')
+        print(f"Logged on as {self.user}")
 
     @tasks.loop(seconds=60)
     async def checkXTask(self):
-        channel = self.get_channel(int(channelID))
+        for target in targets:
+            # https://github.com/HoloArchivists/twspace-dl
+            result = run(f"twspace_dl --skip-download --input-cookie-file cookie.co --user-url {target['url']} --url", stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 
-        # https://github.com/HoloArchivists/twspace-dl
-        result = run(f'twspace_dl --skip-download --input-cookie-file cookie.co --user-url {USER_URL} --url', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+            if result.stderr != '':
+                print(f"[{target['name']}] {result.stderr}")
 
-        if result.stderr != '':
-            print(result.stderr)
+            elif result.stdout != '':
+                print(f"[{target['name']}] {result.stdout}")
 
-        elif result.stdout != '':
-            print(result.stdout)
+                if result.stdout.rstrip().endswith('.m3u8') and target['lastMessage'] != result.stdout:
+                    target['lastMessage'] = result.stdout
 
-            if result.stdout.rstrip().endswith('.m3u8') and self.lastMessage != result.stdout:
-                self.lastMessage = result.stdout
-
-                embed = discord.Embed(colour=int(1053978))
-                embed.set_author(name=USER_NAME)
-                embed.title = 'XSpaceNotifier'
-                embed.description = result.stdout
-                await channel.send(embed=embed)
+                    embed = discord.Embed(colour=int(1053978))
+                    embed.set_author(name=target['name'])
+                    embed.title = 'XSpaceNotifier'
+                    embed.description = result.stdout
+                    
+                    channel = self.get_channel(int(target['channelID']))
+                    await channel.send(embed=embed)
 
     @checkXTask.before_loop
     async def before_checkXTask(self):
@@ -43,10 +39,17 @@ class DiscordClient(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 
-handle = open('discord.auth', 'r')
-discordAuth = handle.readline()
-channelID = handle.readline()
-handle.close()
+targets = [];
+with open('discord.auth', 'r') as handle:
+    discordAuth = handle.readline()
+    
+    while True:
+        line = handle.readline()
+        if not line:
+            break
+        
+        line = line.strip().split(' ')
+        targets.append(dict(url = line[0], name = line[1], channelID = line[2], lastMessage = '' ))
 
 client = DiscordClient(intents=intents)
 client.run(discordAuth)
